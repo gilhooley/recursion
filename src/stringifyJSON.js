2fbc7e03ae562eaae21841383d2b3994d56d9438
KAG <k.gilhooley@gmail.com>
2014-09-01 23:21:55 -0700

    recursive! sort of working!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,17 +1,33 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
-
-
 var stringifyJSON = function(obj) {
 
   var result = [];
-  var myKeys = Object.keys(obj);
-  for (var i = 0; i < myKeys.length; i++) {
-      result.push('"' + myKeys[i] + '": "' + obj[myKeys[i]] + '"');
+  var result2;
+  var sayNull = "null";
+  if (typeof obj === "boolean" || typeof obj === "number") { return obj.toString(); }
+  else if (typeof obj === "string") {return '"' + obj + '"';}
+  else if (obj === null) {return sayNull;}
+
+  else if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+        result.push('"' + stringifyJSON(obj[i]) + '"' );
+    }
+  result2 = "[" + result.join(",") + "]";
+  return result2;
   }
 
-  var result2 = '"{' + result.join(",") + '}"';
+
+  else if (typeof obj === "object" && obj !== null) {
+    for (var key in obj) {
+        result.push('"' + 
+        stringifyJSON(key) + '": '  + stringifyJSON(obj[key]) + '"' );
+    }
+  result2 = "{" + result.join(",") + "}";
   return result2;
+}
+
 
 };
+
