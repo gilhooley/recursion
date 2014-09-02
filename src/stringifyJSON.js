11dccd16d5e84b11878947b076ced4e963f867ca
KAG <k.gilhooley@gmail.com>
2014-09-01 18:56:08 -0700

    fixing bad var name and redundant func declaration

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,13 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj)
+
+  var result = [];
+  var myKeys = Object.keys(obj);
+  for (var i = 0; i < myKeys.length; i++) {
+      result.push('"' + myKeys[i] + '": "' + obj[myKeys[i]] + '"');
+  }
+  var result2 = '"{' + result.join(",") + '}"';
+  return result2;
 };
